6a54ae78124272e4999c0653802c1a75f502d006
Solraz <joshtreestum@gmail.com>
2023-11-29 19:03:47 -0300

    MASONRY: Finishing touches on Masonry

---
+++ static/js/modules/masonry.js
@@ -0,0 +1,31 @@
+const resize_masonry_item = (item) => {
+	let grid = document.querySelector(`masonry-perfect`);
+
+	let rowGap = parseInt(
+		window.getComputedStyle(grid).getPropertyValue("row-gap")
+	);
+
+	let rowSpan = Math.ceil(
+		item.querySelector("masonry-content > img").getBoundingClientRect().height /
+			rowGap
+	);
+
+	item.style.gridRowEnd = "span " + rowSpan;
+};
+
+const resize_all_masonry_items = () => {
+	let all_masonry_bricks = document.querySelectorAll("masonry-brick");
+
+	for (var i = 0; i < all_masonry_bricks.length; i++) {
+		resize_masonry_item(all_masonry_bricks[i]);
+	}
+};
+
+// const wait_for_images = () => {
+// 	let all_masonry_bricks = document.querySelectorAll("masonry-brick");
+
+// 	for (var i = 0; i < all_masonry_bricks.length; i++) {
+// 	}
+// };
+
+export { resize_masonry_item, resize_all_masonry_items };
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,9 @@
 import { navbar } from "./modules/navbar.js";
+import { resize_all_masonry_items } from "./modules/masonry.js";
 
 window.addEventListener("load", () => {
 	navbar();
+	resize_all_masonry_items();
 });
+
+window.addEventListener("resize", () => {});
