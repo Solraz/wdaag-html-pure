54be0d10d933c2c6c8ce39fc7e4c914802ed1964
Solraz <joshtreestum@gmail.com>
2023-12-11 02:05:20 -0300

    NAVBAR: Start of navbar

--- static/js/modules/navbar.js
+++ static/js/modules/navbar.js
@@ -1,5 +1,8 @@
 const navbar = () => {
 	let nav = document.querySelector(`nav`);
+	let opener = nav.querySelector(`opener`);
+
+	
 };
 
 export { navbar };
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -16,13 +16,13 @@ window.addEventListener("load", () => {
 		// sticky_text();
 	});
 
-	let current_head = document.querySelector(`head`);
-	htmx.on("htmx:afterOnLoad", (e) => {
-		console.log(e);
-		// let new_head = document.querySelector(`head`);
+	// let current_head = document.querySelector(`head`);
+	// htmx.on("htmx:afterOnLoad", (e) => {
+	// 	console.log(e);
+	// let new_head = document.querySelector(`head`);
 
-		// current_head.innerHTML = new_head.innerHTML;
-	});
+	// current_head.innerHTML = new_head.innerHTML;
+	// });
 });
 
 window.addEventListener("resize", () => {});
